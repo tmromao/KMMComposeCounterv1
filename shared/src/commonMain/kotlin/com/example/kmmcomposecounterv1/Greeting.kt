4209105f6b33436827f9e5310e4729e24fbd26f1
tromao <tiagomromao@gmail.com>
2023-07-18 22:15:04 +0100

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/example/kmmcomposecounterv1/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.kmmcomposecounterv1
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
